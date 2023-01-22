9fcec3e3b5ba268ad8b5fa64bddd8f5ec2a086be
Picasso <thecodepicasso@gmail.com>
2023-01-22 07:24:32 -0800

    built error handler

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { json, urlencoded } from "body-parser";
 import mongoose from "mongoose";
 const app = express();
@@ -13,6 +13,15 @@ declare global {
     status?: number;
   }
 }
+
+app.use(
+  (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    if (error.status) {
+      return res.status(error.status).json({ message: error.message });
+    }
+    res.status(500).json({ message: "something went wrong" });
+  }
+);
 
 const start = async () => {
   if (!process.env.MONGO_URI) throw new Error(" Mongo URI is required");
