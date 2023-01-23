d8adc6f8449410848798a434d0ce3027821f0ab1
Picasso <thecodepicasso@gmail.com>
2023-01-23 01:01:16 -0800

    finished new comment router

---
+++ src/routers/comment/new.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response, NextFunction } from "express";
+import Comment from "src/models/comment";
+import Post from "src/models/post";
+
+const router = Router();
+
+router.post(
+  "./api/comment/new:postId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userName, content } = req.body;
+    const { postId } = req.params;
+
+    if (!content) {
+      const error = new Error("content is required") as CustomError;
+      error.status = 400;
+      return next(error);
+    }
+
+    const newComment = new Comment({
+      userName: userName ? userName : "anonymous",
+      content,
+    });
+    await newComment.save();
+
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: postId },
+      { $push: { comments: newComment } },
+      { new: true }
+    );
+
+    res.status(201).send(updatedPost);
+  }
+);
+
+export { router as newCommentRouter };
